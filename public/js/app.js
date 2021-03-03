ceae730fb0048a02e4e6acee9a5ff833c22d9e78
anasswalha95 <anas.swalha@yahoo.com>
2021-03-03 13:27:35 +0200

    lab 14 done

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+
+    if ($('#nav').css('visibility') == 'hidden')
+        $('#nav').css('visibility', 'visible');
+    else
+        $('#nav').css('visibility', 'hidden');
+
+}
